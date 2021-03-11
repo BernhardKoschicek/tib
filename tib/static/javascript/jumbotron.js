a50764f4e3c5795f3297f048b2dface6be935727
BernhardKoschicek <b.koschicek@gmail.com>
2021-03-11 12:05:58 +0100

    added navbar

--- tib/static/javascript/jumbotron.js
+++ tib/static/javascript/jumbotron.js
@@ -3,3 +3,11 @@ $(".jumbotron").css({ height: $(window).height() + "px" });
 $(window).on("resize", function() {
   $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+
+
+
+$(document).ready(function(){
+  $('.nav-button').click(function(){
+	$('body').toggleClass('nav-open');
+  });
+});

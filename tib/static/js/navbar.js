6347f7e3db460dd118a8f1a2a2eeed2555a7eb7f
BernhardKoschicek <b.koschicek@gmail.com>
2022-04-21 14:40:35 +0200

    frontpage, fonts and js

--- tib/static/js/navbar.js
+++ tib/static/js/navbar.js
@@ -1,8 +1,13 @@
 $(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navigation");
-    var $img = $(".brand-image");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $img.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+    $(document).scroll(function () {
+        var $nav = $(".navigation");
+        var $navtib = $(".navigation-tib");
+        var $img = $(".brand-image");
+        var $imgtib = $(".brand-image-tib");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $img.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $navtib.toggleClass('scrolled', $(this).scrollTop() > $navtib.height());
+        $imgtib.toggleClass('scrolled', $(this).scrollTop() > $navtib.height());
+    });
 });
+

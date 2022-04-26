21499b72df9035094e6f8f1dafb415d562ec77ac
BernhardKoschicek <b.koschicek@gmail.com>
2022-04-26 08:16:53 +0200

    added team data

---
+++ tib/data/tib/team.py
@@ -0,0 +1,106 @@
+tib_team = {
+    'akulzer': {
+        'name': 'Andreas Külzer',
+        'team': 'asia_minor',
+        'category': 'project_leader',
+        'description': 'Univ.-Prof. Mag. Dr. phil. 1990: Magister Artium, 1993: Dr. phil. (summa cum laude), 1999: professorial qualification (Habilitation), 2007: adjunct Professor (apl. Univ.-Professor) of Byzantine History and Literature at the University of Cologne. Board member of the Austrian Byzantine Society, Vienna; Member of the Austrian National Committee of the Association Internationale des Études Byzantines.<br>Research Interests: Western Anatolia from Early Roman times to the Ottoman period – Harbors and maritime networks in the Eastern Mediterranean – Historical Geography of Asia Minor and the Balkans – Late Antique and Medieval Pilgrimages – Late Antiquity – Ephesus.',
+        'e-mail': 'andreas.kuelzer@oeaw.ac.at',
+        'academia': 'oeaw.academia.edu/AndreasKuelzer'
+    },
+    'darianztzi': {
+        'name': 'Despoina Ariantzi',
+        'team': 'asia_minor',
+        'category': 'scientific_employee',
+        'description': 'Mag. phil. with distinction (University of Ioannina 2001); Dr. phil. with distinction (University of Vienna, 2009), lecturer in Byzantine History at the University of Nicosia/Cyprus (2010-2011); at the University of Vienna (SS 2015); at the National and Kapodistrian University of Athen (SS 2018); PostDoc (scientific_employee) 2011-14; (University of Vienna); project_leader at the RGZM Mainz/Germany (2015-2016); PostDoc (scientific_employee) 2016-2020 (Univeristy of Vienna).<br>Research Interests: Social and cultural history of the Byzantine Empire – History of Childhood – Coming of Age; History of Family – Hagiography and monasticism – Pilgrimage – Marginalization and subculture groups (Prostitutes, actors and tavern- keepers) – Gender Studies.',
+        'e-mail': 'despoina.ariantzi@oeaw.ac.at',
+        'academia': 'https://independent.academia.edu/DAriantzi'
+    },
+    'mpopovic': {
+        'name': 'Mihailo Popović',
+        'team': 'tib_balkans',
+        'category': 'project_leader',
+        'description': 'Priv.-Doz. Mag. Dr. phil.; 2000: Mag. phil. with highest distinction (University of Vienna, 2000); Dr. phil. with highest distinction (University of Vienna, 2005); Unanimous decision to be granted a licence to teach "History of South-East Europe and Byzantine Studies" (University of Vienna, 2011); since 12 January 2017: Chair of the "Commission for the Historical Geography and Spatial Analysis of Byzantium" at the International Association of Byzantine Studies (AIEB).<br>Research Interests: History and Culture of South Eastern Europe (7th-16th Centuries), Late Byzantine History (1204-1453), Gender Studies, Historical Geography and Cartography of the Mediterranean, Digital Humanities and Historical Geographic Information Systems (HGIS)',
+        'e-mail': 'Mihailo.Popovic@oeaw.ac.at',
+        'academia': 'oeaw.academia.edu/MihailoPopovic'
+    },
+    'bkoschicek': {
+        'name': 'Bernhard Koschićek-Krombholz',
+        'team': 'tib_balkans',
+        'category': 'software_developer',
+        'description': 'BSc in Computer Sciences, BA in History, student of Computer Sciences at the University of Applied Sciences Technikum Wien and currently student of history at the University of Vienna. Research interests include computer security, python, Linked Open Data, API, digital reservation, historical geographie, GIS, medieval and military history.',
+        'e-mail': 'Bernhard.Koschicek@oeaw.ac.at',
+        'academia': 'https://oeaw.academia.edu/BernhardKoschicek'
+    },
+    'dvargova': {
+        'name': 'Dorota Vargová',
+        'team': 'tib_balkans',
+        'category': 'scientific_employee',
+        'description': '2016 BA in History (University of Vienna); 2016-2021 MA in History (University of Vienna); Research interest: european history, early-modern history, history of the european identity, history of the middle ages and late antiquity .',
+        'e-mail': 'Dorota.Vargova@oeaw.ac.at',
+        'academia': 'https://independent.academia.edu/DorotaVargov%C3%A1'
+    },
+    'vzervan': {
+        'name': 'Vratislav Zervan',
+        'team': 'tib_balkans',
+        'category': 'scientific_employee',
+        'description': 'MA, PhD; 2006: Master Degree in Archaeology and History; 2010: Master Degree in Classical Philology (Faculty of Arts, Comenius University, Bratislava); 2014: PhD in General History (Faculty of Arts, Comenius University, Bratislava);<br>Research Interests: Byzantine History; Byzantine and Slavonic Literature; Archaeology; Historical Geography; Lexicography',
+        'e-mail': 'Vratislav.Zervan@oeaw.ac.at',
+        'academia': 'oeaw.academia.edu/vratislavzervan'
+    },
+    'jkoder': {
+        'name': 'Johannes Koder',
+        'team': 'tib_editor',
+        'category': 'editor_of_tib',
+        'description': 'w.M., Prof. Dr.; 1960-1965 Byzantine, Arab and Classical Studies in Vienna University; 1965 Dr. phil. (Herbert Hunger); postdoctoral studies in Athens and in Munich (Alexander von Humboldt-Scholarship, 1970/71 and 1974, Hans-Georg Beck), 1973 "Habilitation". 1977 assoc. Professor (University of Vienna); 1978-1985 Professor and director of the department for Byzantine Studies, Johannes Gutenberg Universität, Mainz, Germany; 1985-2010 Professor and 1987-1989/1991-1993/1995-2010 Director of the Institute for Byzantine and Neohellenic Studies, University of Vienna. Austrian Archaeological Institute, interim director, 2007-2009; director of excavations, Ephesos, 2008-2009.<br>Austrian Academy of Sciences: corresponding member 1989, 2000 full member, director of the research units "Tabula Imperii Byzantini" (until 2005) and "Balkan-Kommission" (until 2007), chair Advisory Board Institut für Byzanzforschung (2006-2008); Austrian Federal Board of Professors of Universities, member 1991, president 1993-2003; Austrian Association for Byzantine Studies, member 1963, president 1996-2014; Association Internationale des Études Byzantines, vice-president 1986, president 2012-2016; Dumbarton Oaks Research Library and Collection (Washington). senior fellow Byzantine studies, 2002-2008 (chair 2005-2008);<br>Academy of Athens, foreign member 2007; Academia Europaea, member (2012); Dr. h. c. University of Athens (2006), University of Ioannina (2011), University of Thrake (2016); Full member, Austrian Archaeological Institute (2010); Distinctions: "Commander of the Order of the Phoenix" of the Hellenic Republic (1996), „Distinguished Decoration in Gold for Service to the Republic of Austria" (2004), "Cross of Merit for Science and Art", 1st class, Republic of Austria (2010) .',
+        'e-mail': 'Johannes.Koder@oeaw.ac.at',
+        'academia': 'https://independent.academia.edu/JohannesKoder'
+    },
+    'vpolloczek': {
+        'name': 'Veronika Polloczek',
+        'team': 'associeted_scholars',
+        'category': 'scientific_employee',
+        'description': 'Mag.a in Classics, Student of MA Program in Historical Research, Auxiliary Sciences and Archival Studies at the University of Vienna. Research interests Medieval Sources, Middle and Neo Latin, Palaeography, Digitalization of Sources, Heraldry.',
+        'e-mail': 'Veronika.Polloczek@oeaw.ac.at',
+        'academia': 'https://oeaw.academia.edu/VeronikaPolloczek'
+    },
+    'mhernandez': {
+        'name': 'Moisés Hernández Cordero',
+        'team': 'asia_minor',
+        'category': 'spatial_analyst',
+        'description': 'Degree History 2007 Universidad Autonónoma de Madrid (UAM) MSc, 2011: Master Degree in GIS and Spatial Analysis in Archaeology (Institute of Archaeology, University College London, London) Research Interests: GIS and Spatial Analysis, Geomatics applications in Archaeology, Structure from Motion, UV IR and Multispectral imagery, Deep Learning and Machine Learning for Cultural research.',
+        'e-mail': 'moises.hernandez.cordero@oeaw.ac.at',
+        'academia': 'https://oeaw.academia.edu/MoisesHernandez'
+    },
+    'dschmid': {
+        'name': 'David Schmid',
+        'team': 'former_employee',
+        'category': 'scientific_employee',
+        'description': 'BA in History, student of medieval history at the University of Vienna. Research interests include historical geography, GIS, late antique and medieval and military history.',
+        'e-mail': 'david.schmid@oeaw.ac.at',
+        'academia': 'https://univie.academia.edu/DavidSchmid'
+    },
+    'kbelke': {
+        'name': 'Klaus Belke',
+        'team': 'emeriti_scholars',
+        'category': 'scientific_employee',
+        'description': 'Dr. phil. (University of Vienna and Munich, 1976); 1976–2012 Academic collaborator at the Austrian Academy of Sciences; at the Commission for the Tabula Imperii Byzantini; from 2006 Institute for Byzantine Studies; from 2012 Division of Byzantine Studies of the Institute for Medieval Studies). 1999 Habilitation at the Institute for Byzantine and Neoellenic Studies of the University of Vienna. Teaching at the University of Vienna; 1999, 2001 teaching at the Central European University at Budapest. 2012 retirement after reaching the legal age of retirement. Continuing voluntarily work for the Tabula Imperii Byzantine project.<br>Research Interests: Historical Geography of the Byzantine Empire, esp. Asia Minor Road, means of traffic in the Roman and Byzantine periods',
+        'e-mail': 'Klaus.Belke@oeaw.ac.at',
+        'academia': 'oeaw.academia.edu/KlausBelke'
+    },
+    'fhild': {
+        'name': 'Friedrich Hild',
+        'team': 'emeriti_scholars',
+        'category': 'scientific_employee',
+        'description': 'Dr. phil.; Studies of Classical Philology, Ancient History, Ancient Numismatics and Byzantine Studies (1960–1968) at the University of Vienna; Dr. phil. in Ancient History. From 1969 scholarly co-worker in the "Kommission für die Tabula Imperii Byzantini / Commission of the Tabula Imperii Byzantini"; 1977-1987: deputy chairman of the TIB. From 1976 until 1986 secretary and treasurer and from 1987 until 1996 treasurer of the "Austrian Byzantine Society / Österreichische Byzantinische Gesellschaft". After his retirement in 2007 he is working as honorary (emeritus) scholar for the TIB.<br>Research Interests: Ancient and Byzantine History, Archaeology, Historical Geography and Cartography of the Mediterranean',
+        'e-mail': 'Friedrich.Hild@oeaw.ac.at',
+        'academia': 'https://oeaw.academia.edu/friedrichhild'
+    },
+    'psoustal': {
+        'name': 'Peter Soustal',
+        'team': 'emeriti_scholars',
+        'category': 'scientific_employee',
+        'description': 'Mag. Dr. phil.; Mag. phil. (University of Vienna, 1969); Dr. phil. (University of Vienna, 1975); employed at the Austrian Academy of Sciences since 1969, from 1973 at the department of Tabula Imperii Byzantini; from 2006 until 2012 director of the former Institute for Byzantine Research of the Austrian Academy of Sciences; since the retirement in 2012 voluntary collaborator at the Institute for Medieval Research, Division of Byzantine Research.<br>Research Interests: Historical Geography of the Southern part of the Balkan peninsula (Albania, Northern Greece, Bulgaria), historical Studies in Place names.',
+        'e-mail': 'Peter.Soustal@oeaw.ac.at',
+        'academia': 'oeaw.academia.edu/PeterSoustal'
+    }
+}

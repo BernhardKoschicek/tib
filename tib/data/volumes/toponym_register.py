049a06481992b32c62ac87d97d634664d7ad0c91
BernhardKoschicek <b.koschicek@gmail.com>
2022-05-16 15:28:22 +0200

    begin register

--- tib/data/volumes/toponym_register.py
+++ tib/data/volumes/toponym_register.py
@@ -22,6 +22,91 @@ register_volume = {
     'tib1': {
         'title': 'TIB 1 Hellas and Thessaly',
         'icon': '',
-
-    }
+        'published': True,
+    },
+    'tib2': {
+        'title': 'TIB 2 Cappadocia (Kappadokia, Charsianon, Sebasteia and Lykandos)',
+        'icon': '',
+        'published': True,
+    },
+    'tib3': {
+        'title': 'TIB 3 Nicopolis and Cephalonia',
+        'icon': '',
+        'published': True,
+    },
+    'tib4': {
+        'title': 'TIB 4 Galatia and Lycaonia',
+        'icon': '',
+        'published': True,
+    },
+    'tib5': {
+        'title': 'TIB 5 Cilicia and Isauria',
+        'icon': '',
+        'published': True,
+    },
+    'tib6': {
+        'title': 'TIB 6 Thrace (Thrakē, Rodopē and Haimimontos)',
+        'icon': '',
+        'published': True,
+    },
+    'tib7': {
+        'title': 'TIB 7 Phrygia and Pisidia',
+        'icon': '',
+        'published': True,
+    },
+    'tib8': {
+        'title': 'TIB 8 Lycia and Pamphylia',
+        'icon': '',
+        'published': True,
+    },
+    'tib9': {
+        'title': 'TIB 9 Paphlagonia and Honorias',
+        'icon': '',
+        'published': True,
+    },
+    'tib10': {
+        'title': 'TIB 10 Nothern Aegean',
+        'icon': '',
+        'published': True,
+    },
+    'tib11': {
+        'title': 'TIB 11 Macedonia, Southern Part',
+        'icon': '',
+        'published': True,
+    },
+    'tib12': {
+        'title': 'TIB 12 Eastern Thrace (Eurōpē)',
+        'icon': '',
+        'published': True,
+    },
+    'tib13': {
+        'title': 'TIB 13 Bithynia and Hellespont',
+        'icon': '',
+        'published': True,
+    },
+    'tib14': {
+        'title': 'TIB 14 Western Asia Minor: Lydia and Asia',
+        'icon': '',
+        'published': True,
+    },
+    'tib15': {
+        'title': 'TIB 15 Syria (Syria Prōtē, Syria Deutera, Syria Euphratēsia)',
+        'icon': '',
+        'published': True,
+    },
+    'tib16': {
+        'title': 'TIB 16 Macedonia, Northern Part',
+        'icon': '',
+        'published': True,
+    },
+    'tib17': {
+        'title': 'TIB 17 Nea Epeiros and Praevalis',
+        'icon': '',
+        'published': True,
+    },
+    'tib18': {
+        'title': 'TIB 18 Caria',
+        'icon': '',
+        'published': True,
+    },
 }

c50fc2f7f4b0b82e0ee0e4422a9aed5b125ecf3b
BernhardKoschicek <b.koschicek@gmail.com>
2022-05-19 09:50:53 +0200

    borderzones

--- tib/data/tib/presentations.py
+++ tib/data/tib/presentations.py
@@ -25,16 +25,184 @@ presentations = [{
     "category": ["holdura"]
 }, {
     "presenter": "Markus Breier",
-    "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes ",
+    "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes",
     "conference": "International Medieval Congress, University of Leeds",
     "date": datetime(2021, 7, 7).strftime('%d/%m/%Y'),
     'location': 'Leeds',
     "category": ["holdura"]
 }, {
     "presenter": "Markus Breier, Karel Kriz, Lukas Neugebauer, Alexander Pucher",
-    "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes ",
+    "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes",
     "conference": "30th International Cartographic Conference 2021",
     "date": datetime(2021, 12, 18).strftime('%d/%m/%Y'),
     'location': 'Florenz',
     "category": ["holdura"]
-}]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "Kritische Bemerkungen zu den Briefen Karls des IV. an Stefan Dušan und an Ioannes V. Paleologos",
+    "conference": "Byzanz und das Abendland VI, Eötvös-József-Collegium",
+    "location": "Budapest",
+    "date": "17. April 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Two Precious Charters from the 14th Century and Their Digital Footprint",
+    "conference": "Workshop Archéologie, histoire et analyse spatiale. Dialogue interdisciplinaire sur la question des SIG archéologiques et historiques",
+    "location": "Lille",
+    "date": "31. Mai 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Two Documents from the Hilandar Research Library and Their Digital Footprint",
+    "conference": "7th International Hilandar Conference „Preserving and Accessing Medieval Slavic Manuscripts“, The Ohio State University, Columbus",
+    "location": "Ohio, USA",
+    "date": "15. Juni 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "Border Warlords as Founders and Donators: Memory-Keeping in Monasteries and Churches of Byzantine Macedonia",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Bernhard Koschiček",
+    "title": "Digital Memory-Keeping of Border. Warlords in Byzantine Macedonia in the OpenAtlas Database",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Memories are Made of This: Tracing Local Elites in Byzantine Macedonia",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Big Data in Byzantium? Thoughts on the Analysis of Byzantine Regions from the Viewpoint of Historical Geography and Digital Humanities",
+    "conference": "Central European University – CEU",
+    "location": "Budapest",
+    "date": "10. Oktober 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Тумачење садржаја средњовековних повеља из угла историјске географије и дигиталне хуманистике",
+    "conference": "Workshop „Дигиталне едиције средњовековних докумената: пројекат Српског дигиталног дипломатара“",
+    "location": "Beograd",
+    "date": "28. November 2018",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "The Myth of Big Data in Byzantium versus One of the Ways of Combining Byzantine / Medieval Studies, Historical Geography and Digital Humanities [Keynote]",
+    "conference": "Četvrta medievistička znanstvena radionica u Rijeci",
+    "location": "Rijeka",
+    "date": "31. Mai 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "The (Im)Material Imperial in a Contested Area: The Attribute 'Tsar' in the Toponymy of Byzantine Macedonia",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "The Material Culture of Everyday Life in the Northern Macedonia at the Turn of the 13th and 14th Centuries: The Case of the Border Warlords",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Bernhard Koschiček",
+    "title": "Tracing Material Culture in Northern Macedonia via Spatial Visualisation (GIS)",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "3. Juli 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Bernhard Koschiček",
+    "title": "Die digitale, geoanalystische Aufarbeitung von Grenzen, Orten und Verkehrswegen im Nordmazedonien des 14. Jahrhunderts: erste Ansätze",
+    "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“",
+    "location": "Wien",
+    "date": "17.-18. Oktober 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "Prosopographische Bemerkungen zu einer Grenzregion in Zeiten der Krise: Herausragende Akteure und Opfer nach den Eroberungszügen des serbischen Königs Stefan Uroš II. Milutin im byzantinischen Makedonien",
+    "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“",
+    "location": "Wien",
+    "date": "17.-18. Oktober 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Über das Verhältnis von Herrschertiteln und Räumen: eine Fallstudie zu Makedonien unter dem König und Zaren Stefan Dušan",
+    "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“",
+    "location": "Wien",
+    "date": "17.-18. Oktober 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "The Appropriation of Byzantine Macedonia by the King and Emperor Stefan Uroš IV Dušan: Means and Outcome",
+    "conference": "St. Petersburg International Historical Forum",
+    "location": "St. Petersburg",
+    "date": "29. Oktober-3. November 2019",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović, Bernhard Koschiček",
+    "title": "Roads of Conquest, Roads of Destruction: Settlement Desertion as Mirrored in the Byzantino-Serbian Antagonism in Byzantine Macedonia (13th/14th Centuries)",
+    "conference": "International Digital Conference, Mapping Settlement Desertion in Southeastern Europe from Antiquity to the Modern Era",
+    "location": "Athen",
+    "date": "22.-23. April 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Espace et peuplement en Macédoine byzantine de l’Antiquité tardive à la fin du royaume serbe : l’apport de la Tabula Imperii Byzantini [Keynote]",
+    "conference": "Académie des Inscriptions et Belles-Lettres",
+    "location": "Paris",
+    "date": "Séance du 28 mai 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Current State and Outlook of the Research of the TIB Balkans: TIB Volumes, Digital Humanities and Geocommunication",
+    "conference": "Seventh National Conference of Byzantine Studies, Serbian Committee for Byzantine Studies",
+    "location": "Beograd",
+    "date": "24. Juni 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "Microregional Change of Means on the Byzantino-Serbian Border during the 14th Century: New Insight into the Register of Property of the Church of Saint Stephen in the Village of Konče",
+    "conference": "Seventh National Conference of Byzantine Studies",
+    "location": "Beograd",
+    "date": "24. Juni 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "The Perception of Space in the Serbian Medieval Realm under the Kings Stefan Milutin and Stefan Dečanski, 1282-1330",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "7. Juli 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "Migration within, into, and from the Border Zones: A Case Study on Byzantine Macedonia in Medieval Times, 13th-14th Centuries",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "7. Juli 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Bernhard Koschiček",
+    "title": "The View of Borders: An Approach to (Re)Creating Medieval Border Zones in Byzantine Macedonia with Spatial Analysis 1282-1355",
+    "conference": "International Medieval Congress, University of Leeds",
+    "location": "Leeds",
+    "date": "7. Juli 2021",
+    "category": ["borderzones"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Separating the Wheat from the Chaff: On the Use and Usefulness of Digital Humanities in History and Beyond",
+    "conference": "Searching Digital: Methods, Tools, and Standards of Research in Digital Humanities, Online workshops organised by the Centre for the Study of the Balkans, Department of History, Goldsmiths University of London, CHASE Doctoral Training Partnership, Session",
+    "location": "London",
+    "date": "21. Juni 2021",
+    "category": ["borderzones"]}
+]

37a44c3dd68362dc69fe1a6b0679a63876834d12
BernhardKoschicek <b.koschicek@gmail.com>
2022-05-20 10:00:24 +0200

    3dobjekte

--- tib/data/digital.py
+++ tib/data/digital.py
@@ -1,18 +1,18 @@
 objects3d = {
     "montenegro-relief": {
-        "title": "Relief Montenegro",
+        "title": "Reliefkarte von Montenegro",
         "icon": "images/boxes_background/cetinje_relief_icon.jpg",
         "icon_description": "",
         "path": "3dhop/relief.html",
     },
     "icon": {
-        "title": "Maria Ikone",
+        "title": "Ikone der Mutter Maria",
         "icon": "images/boxes_background/icon_icon.jpg",
         "icon_description": "",
         "path": "3dhop/icon.html",
     },
     "mitra": {
-        "title": "Mitra des Decanski",
+        "title": "Mitra",
         "icon": "images/boxes_background/mitra_icon.jpg",
         "icon_description": "",
         "path": "3dhop/mitra.html",

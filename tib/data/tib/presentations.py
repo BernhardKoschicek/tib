927b347fc019f628f7783cf67840d6da3547dd7e
BernhardKoschicek <b.koschicek@gmail.com>
2022-07-25 16:05:32 +0200

    added imc and fixed sort issue

--- tib/data/tib/presentations.py
+++ tib/data/tib/presentations.py
@@ -4,253 +4,316 @@ presentations = [{
     "presenter": "Mihailo St. Popović",
     "title": "Historical Geography, Digital Humanities and Database Systems: an Approach to reconstruct “Sacred Landscapes” – the Case of Medieval Duklja and Raška (today’s Montenegro and Serbia)",
     "conference": "Global Eurasia, Workshop II: Handlungsspielräume, Netzwerke und transregionale Kontexte",
-    "date": datetime(2022, 5, 12).strftime('%d/%m/%Y'),
     'location': 'Wien',
+    "date": datetime(2022, 5, 12).strftime('%Y/%m/%d'),
     "category": ["holdura"]
 }, {
     "presenter": "Mihailo St. Popović",
     "title": "On the Use and Usefulness of Digital Humanities in the Historical Geography of Byzantium",
     "conference": "Seminar Series: The Balkans Between Empires, Aristotle University of Thessaloniki, Ibn Haldun University Istanbul",
-    "date": datetime(2022, 4, 1).strftime('%d/%m/%Y'),
     'location': 'Thessaloniki',
+    "date": datetime(2022, 4, 1).strftime('%Y/%m/%d'),
     "category": ["holdura"]
 }, {
     "presenter": "Markus Breier",
     "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes",
     "conference": "International Medieval Congress, University of Leeds",
-    "date": datetime(2021, 7, 7).strftime('%d/%m/%Y'),
     'location': 'Leeds',
+    "date": datetime(2021, 7, 7).strftime('%Y/%m/%d'),
     "category": ["holdura"]
 }, {
     "presenter": "Markus Breier, Karel Kriz, Lukas Neugebauer, Alexander Pucher",
     "title": "Beyond East and West – Geocommunicating Historical Sacred Landscapes",
     "conference": "30th International Cartographic Conference 2021",
-    "date": datetime(2021, 12, 18).strftime('%d/%m/%Y'),
     'location': 'Florenz',
+    "date": datetime(2021, 12, 18).strftime('%Y/%m/%d'),
     "category": ["holdura"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "Kritische Bemerkungen zu den Briefen Karls des IV. an Stefan Dušan und an Ioannes V. Paleologos",
     "conference": "Byzanz und das Abendland VI, Eötvös-József-Collegium",
     "location": "Budapest",
-    "date": datetime(2018, 4, 17).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 4, 17).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Two Precious Charters from the 14th Century and Their Digital Footprint",
     "conference": "Workshop Archéologie, histoire et analyse spatiale. Dialogue interdisciplinaire sur la question des SIG archéologiques et historiques",
     "location": "Lille",
-    "date": datetime(2018, 5, 31).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 5, 31).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Two Documents from the Hilandar Research Library and Their Digital Footprint",
     "conference": "7th International Hilandar Conference „Preserving and Accessing Medieval Slavic Manuscripts“, The Ohio State University, Columbus",
     "location": "Ohio, USA",
-    "date": datetime(2018, 6, 15).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 6, 15).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "Border Warlords as Founders and Donators: Memory-Keeping in Monasteries and Churches of Byzantine Macedonia",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2018, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Bernhard Koschiček",
     "title": "Digital Memory-Keeping of Border. Warlords in Byzantine Macedonia in the OpenAtlas Database",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2018, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Memories are Made of This: Tracing Local Elites in Byzantine Macedonia",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2018, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Big Data in Byzantium? Thoughts on the Analysis of Byzantine Regions from the Viewpoint of Historical Geography and Digital Humanities",
     "conference": "Central European University – CEU",
     "location": "Budapest",
-    "date": datetime(2018, 10, 10).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 10, 10).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Тумачење садржаја средњовековних повеља из угла историјске географије и дигиталне хуманистике",
     "conference": "Workshop „Дигиталне едиције средњовековних докумената: пројекат Српског дигиталног дипломатара“",
     "location": "Beograd",
-    "date": datetime(2018, 11, 28).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 11, 28).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "The Myth of Big Data in Byzantium versus One of the Ways of Combining Byzantine / Medieval Studies, Historical Geography and Digital Humanities [Keynote]",
     "conference": "Četvrta medievistička znanstvena radionica u Rijeci",
     "location": "Rijeka",
-    "date": datetime(2019, 5, 31).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 5, 31).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "The (Im)Material Imperial in a Contested Area: The Attribute 'Tsar' in the Toponymy of Byzantine Macedonia",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2019, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "The Material Culture of Everyday Life in the Northern Macedonia at the Turn of the 13th and 14th Centuries: The Case of the Border Warlords",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2019, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Bernhard Koschiček",
     "title": "Tracing Material Culture in Northern Macedonia via Spatial Visualisation (GIS)",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2019, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 7, 3).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Bernhard Koschiček",
     "title": "Die digitale, geoanalystische Aufarbeitung von Grenzen, Orten und Verkehrswegen im Nordmazedonien des 14. Jahrhunderts: erste Ansätze",
     "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“, 17.-18. Oktober 2019",
     "location": "Wien",
-    "date": datetime(2019, 10, 18).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 10, 18).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "Prosopographische Bemerkungen zu einer Grenzregion in Zeiten der Krise: Herausragende Akteure und Opfer nach den Eroberungszügen des serbischen Königs Stefan Uroš II. Milutin im byzantinischen Makedonien",
     "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“, 17.-18. Oktober 2019",
     "location": "Wien",
-    "date": datetime(2019, 10, 17).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 10, 17).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Über das Verhältnis von Herrschertiteln und Räumen: eine Fallstudie zu Makedonien unter dem König und Zaren Stefan Dušan",
     "conference": "Arbeitstreffen „Byzantino-Serbian Border Zones in Transition: Migration and Elite Change in pre-Ottoman Macedonia“, 17.-18. Oktober 2019",
     "location": "Wien",
-    "date": datetime(2019, 10, 17).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 10, 17).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "The Appropriation of Byzantine Macedonia by the King and Emperor Stefan Uroš IV Dušan: Means and Outcome",
     "conference": "St. Petersburg International Historical Forum, 29. Octobre-3. November 2019",
     "location": "St. Petersburg",
-    "date": datetime(2019, 10, 30).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 10, 30).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović, Bernhard Koschiček",
     "title": "Roads of Conquest, Roads of Destruction: Settlement Desertion as Mirrored in the Byzantino-Serbian Antagonism in Byzantine Macedonia (13th/14th Centuries)",
     "conference": "International Digital Conference, Mapping Settlement Desertion in Southeastern Europe from Antiquity to the Modern Era",
     "location": "Athen",
-    "date": datetime(2021, 4, 23).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 4, 23).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Espace et peuplement en Macédoine byzantine de l’Antiquité tardive à la fin du royaume serbe : l’apport de la Tabula Imperii Byzantini [Keynote]",
     "conference": "Académie des Inscriptions et Belles-Lettres",
     "location": "Paris",
-    "date": datetime(2021, 5, 28).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 5, 28).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Current State and Outlook of the Research of the TIB Balkans: TIB Volumes, Digital Humanities and Geocommunication",
     "conference": "Seventh National Conference of Byzantine Studies, Serbian Committee for Byzantine Studies",
     "location": "Beograd",
-    "date": datetime(2021, 6, 24).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 6, 24).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "Microregional Change of Means on the Byzantino-Serbian Border during the 14th Century: New Insight into the Register of Property of the Church of Saint Stephen in the Village of Konče",
     "conference": "Seventh National Conference of Byzantine Studies",
     "location": "Beograd",
-    "date": datetime(2021, 6, 24).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 6, 24).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "The Perception of Space in the Serbian Medieval Realm under the Kings Stefan Milutin and Stefan Dečanski, 1282-1330",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2021, 7, 7).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 7, 7).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "Migration within, into, and from the Border Zones: A Case Study on Byzantine Macedonia in Medieval Times, 13th-14th Centuries",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2021, 7, 7).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 7, 7).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Bernhard Koschiček",
     "title": "The View of Borders: An Approach to (Re)Creating Medieval Border Zones in Byzantine Macedonia with Spatial Analysis 1282-1355",
     "conference": "International Medieval Congress, University of Leeds",
     "location": "Leeds",
-    "date": datetime(2021, 7, 7).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 7, 7).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Separating the Wheat from the Chaff: On the Use and Usefulness of Digital Humanities in History and Beyond",
     "conference": "Searching Digital: Methods, Tools, and Standards of Research in Digital Humanities, Online workshops organised by the Centre for the Study of the Balkans, Department of History, Goldsmiths University of London, CHASE Doctoral Training Partnership, Session",
     "location": "London",
-    "date": datetime(2021, 7, 21).strftime('%d/%m/%Y'),
+    "date": datetime(2021, 7, 21).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Vratislav Zervan",
     "title": "PLP Addenda: Die byzantinische Elite nach den Eroberungszügen der serbischen Herrscher in Makedonien",
     "conference": "Byzanz und das Abendland/Byzance et l'Occident VIII. Studia Byzantino-Occidentalia. Gallica-Italica-Graeca. Internationale Konferenz/Colloque International. Eötvös-József-Collegium/Collège Eötvös József, 23-25. May",
     "location": "Budapest",
-    "date": datetime(2022, 5, 24).strftime('%d/%m/%Y'),
+    "date": datetime(2022, 5, 24).strftime('%Y/%m/%d'),
     "category": ["borderzones"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "„Zwei Reliefkarten des Balkans – ein ‘ungewöhnlicher’ Blick auf Skopje und auf Montenegro im ersten Viertel des 20. Jahrhunderts“",
     "conference": "DPP Lecture",
     "location": "Wien",
-    "date": datetime(2018, 12, 18).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 12, 18).strftime('%Y/%m/%d'),
     "category": ["montenegro"]
 }, {
     "presenter": "Mihailo Popović",
     "title": "Vortrag über historische Geographie des Balkanraums und des Projektes vor Studierenden der Universität Donja Gorica in Podgorica",
     "conference": "",
     "location": "Montenegro",
-    "date": datetime(2020, 3, 5).strftime('%d/%m/%Y'),
+    "date": datetime(2020, 3, 5).strftime('%Y/%m/%d'),
     "category": ["montenegro"]
 }, {
     "presenter": "Andreas Külzer, Veronika Polloczek, Mihailo St. Popović",
     "title": "Die digitale Tabula Imperii Byzantini (Dig-TIB) als Beitrag zum Weltkulturerbe",
     "conference": "DPP Lecture",
     "location": "Wien",
-    "date": datetime(2018, 3, 22).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 3, 22).strftime('%Y/%m/%d'),
     "category": ["digtib"]
 }, {
     "presenter": "Veronika Polloczek",
     "title": "How to Expand the Tabula Imperii Byzantini in a Digital Age?",
     "conference": "International Medieval Congress",
     "location": "Leeds, Großbritannien",
-    "date": datetime(2018, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 7, 3).strftime('%Y/%m/%d'),
     "category": ["digtib"]
 }, {
     "presenter": "Veronika Polloczek, Mihailo St. Popović",
     "title": "The Digital Tabula Imperii Byzantini (Dig-TIB) as Contribution to the World’s Culture Heritage",
     "conference": "Congress Visual Heritage",
     "location": "Wien",
-    "date": datetime(2018, 11, 12).strftime('%d/%m/%Y'),
+    "date": datetime(2018, 11, 12).strftime('%Y/%m/%d'),
     "category": ["digtib"]
 }, {
     "presenter": "Veronika Polloczek",
     "title": "Digitising and Archiving Cultural Heritage in DPP / OpenAtlas on the Example of the Tabula Imperii Byzantini",
     "conference": "International Medieval Congress",
     "location": "Leeds, Großbritannien",
-    "date": datetime(2019, 7, 3).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 7, 3).strftime('%Y/%m/%d'),
     "category": ["digtib"]
 }, {
     "presenter": "Mihailo St. Popović",
     "title": "Antique and Medieval Cultural Heritage in Macedonia during WW1: Based on German and Austrian Archival Evidence ",
     "conference": "On the Cross-Path of Cultural Ideas: Macedonia, The Balkans, Southeast Europe – Heritage, Management, Resources",
     "location": "Ohrid, Nordmazedonien",
-    "date": datetime(2019, 9, 24).strftime('%d/%m/%Y'),
+    "date": datetime(2019, 9, 24).strftime('%Y/%m/%d'),
+    "category": ["digtib"]
+}, {
+    "presenter": "Dorota Vargová",
+    "title": "An Ecclesiastical History of Medieval Duklja: A Landscape Defined by Rite",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura"]
+}, {
+    "presenter": "Branka Vranešević",
+    "title": "Towards Imperial Dignity: A Contribution to the Study of the Imperial Crown of King Stefan Uroš III Dečanski in the Cetinje Monastery",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura"]
+}, {
+    "presenter": "Moisés Hernández Cordero",
+    "title": "Mapping Cultural Heritage beyond the Eye: The Case of the Crown of the Serbian King Stefan Dečanski",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura", "montenegro"]
+}, {
+    "presenter": "Mihailo Popović",
+    "title": "Beyond East and West: Sacred Landscapes in the Territory of Today's Montenegro, 11th-14th Centuries",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura"]
+}, {
+    "presenter": "Markus Breier",
+    "title": "Modern Cartographic Representations of Medieval Borders and Border Zones",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura"]
+}, {
+    "presenter": "Lukas Neugebauer",
+    "title": "Experiencing Historical Landscapes Using Visual Storytelling",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["holdura"]
+}, {
+    "presenter": "Bernhard Koschiček-Krombholz",
+    "title": "Medieval Borders in the Digital Age: Approaching the (Re)Creation of Medieval Border Zones through Digital Methods (OpenAtlas and GIS)",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["borderzones"]
+}, {
+    "presenter": "Vratislav Zervan",
+    "title": "The Governors of the Border Towns during the Serbian Expansion into Byzantine Macedonia, 1282-1355: Survey and Analysis of Elite Change",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
+    "category": ["borderzones"]
+}, {
+    "presenter": "Veronika Polloczek",
+    "title": "On the Trail of the Digital Tabula Imperii Byzantini: How Changes in the World's Cultural Heritage Become Visible",
+    "conference": "International Medieval Congress",
+    "location": "Leeds, Großbritannien",
+    "date": datetime(2022, 7, 6).strftime('%Y/%m/%d'),
     "category": ["digtib"]
 },
 ]

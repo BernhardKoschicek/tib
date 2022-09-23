1bd2413a1cd0a0b1b26cacb2d3cde44cca40c53e
BernhardKoschicek <b.koschicek@gmail.com>
2022-09-23 14:07:19 +0200

    counter translated

--- tib/data/tib/counter.py
+++ tib/data/tib/counter.py
@@ -1,37 +1,39 @@
+from flask_babel import lazy_gettext as _l
+
 counter = {
     'tib_volumen': {
         'count': 13,
-        'description': 'TIB Volumes Published',
+        'description': _l('TIB Volumes Published'),
         'icon': 'bi-book-fill'
     },
     'tib_making': {
         'count': 5,
-        'description': 'TIB Volumes in Progress',
+        'description': _l('TIB Volumes in Progress'),
         'icon': 'bi-pencil-fill'
     },
     'article': {
         'count': 136,
-        'description': 'Articles published since 2004',
+        'description': _l('Articles published since 2004'),
         'icon': 'bi-body-text'
     },
     'presentations': {
         'count': 235,
-        'description': 'Presentations held since 2006',
+        'description': _l('Presentations held since 2006'),
         'icon': 'bi-chat-left-text-fill'
     },
     'books': {
         'count': 16,
-        'description': 'Books published since 2006',
+        'description': _l('Books published since 2006'),
         'icon': 'bi-book-half'
     },
     'pictures': {
         'count': 100000,
-        'description': 'and more pictures shot since 1966',
+        'description': _l('and more pictures shot since 1966'),
         'icon': 'bi-camera-fill'
     },
     'travels': {
         'count': 68,
-        'description': 'and more travels accomplished',
+        'description': _l('and more travels accomplished'),
         'icon': 'bi-send-fill'
     }
 }

cdda88b8f41b35414cebf5e223472aa28a542803
BernhardKoschicek <b.koschicek@gmail.com>
2022-09-23 13:59:26 +0200

    frontpage and jumbotron

--- tib/data/tib/jumbotron.py
+++ tib/data/tib/jumbotron.py
@@ -1,19 +1,19 @@
+from flask_babel import lazy_gettext as _l
+
 front_jumbotron = {
     'tib_digtib': {
-        'title': 'The TIB in a Digital Age',
-        'text': 'A repository of TIB resources is digitally available',
+        'title': _l('The TIB in a Digital Age'),
+        'text': _l('A repository of TIB resources is digitally available'),
         'image_description': ''
     },
     'tib_current_status': {
-        'title': 'TIB Publications in the Making ',
-        'text': 'The TIB Team is currently working on five new TIB volumes in '
-                'Asia Minor and the Balkans',
+        'title': _l('TIB Publications in the Making'),
+        'text': _l('The TIB Team is currently working on five new TIB volumes in Asia Minor and the Balkans'),
         'image_description': ''
     },
     'tib_sub_projects': {
-        'title': 'Sub Projects of the TIB ',
-        'text': 'The TIB Team works in several sub projects on '
-                'groundbreaking research',
+        'title': _l('Sub Projects of the TIB'),
+        'text': _l('The TIB Team works in several sub projects on groundbreaking research'),
         'image_description': ''
     },
 }
